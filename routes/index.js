64b8c79fd7c0a0760ee8c03917746f9832edb93d
Rachit Pareek <rachit@Rachits-Macbook.local>
2019-08-12 13:18:37 -0700

    Final Kaiser commit

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ router.get('/', function(req, res, next){
     res.render('index.html');
 });
 
-router.get('/jiggy', function(req, res, next){
-    res.send('index.html');
-});
-
 router.get('/data', function(req, res, next){
     userData.find().then( (data) =>
         {
